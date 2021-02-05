f210a650a4189b2c6daba9708df76e736b1b33a8
Abhishek Mittal <abhishek.m@students.iiit.ac.in>
2021-02-05 17:49:01 +0530

    Able to draw a rectangle on the screen using the Screen MAP

--- Week04_HackProgramming/rectangleUsingScreenMap.asm
+++ Week04_HackProgramming/rectangleUsingScreenMap.asm
@@ -1,3 +1,48 @@
-// This code is meant to display a rectangle at the top left of the screen with dimensions 50 pixels long and 16 pixels wide. 
+// This code is meant to display a rectangle at the top left of the screen with dimensions x pixels long and 16*y pixels wide, where x and y are whole numbers
 
+@50
+D=A
+@X
+M=D
+@Y
+M=1
 @16384
+D=A
+@base
+M=D
+
+@i
+M=0
+(LOOP1)
+@j
+M=0
+
+(LOOP2)
+@base
+D=M
+@j // it is the address of the 16bit'th column of the row 
+A=D+M
+M=-1
+@j
+M=M+1
+
+@Y
+D=M
+@j
+D=D-M
+@LOOP2
+D;JGT
+
+@32
+D=A
+@base
+M=M+D // it is the starting address of the row.
+@i
+M=M+1 // it is the index of the row
+
+@X
+D=M
+@i
+D=D-M
+@LOOP1
+D;JGT
